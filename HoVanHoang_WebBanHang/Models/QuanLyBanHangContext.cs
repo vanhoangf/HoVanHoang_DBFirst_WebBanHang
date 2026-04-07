c3025b1a478eca39bccb82d0f7e4031c8cce5fc0
Hồ Hoàng <hovanhoang959@gmail.com>
2026-04-07 21:50:08 +0700

    Project sau khi cap nhat file appsettings.json, Program.cs, QuanLyBanHangContext.cs

--- HoVanHoang_WebBanHang/Models/QuanLyBanHangContext.cs
+++ HoVanHoang_WebBanHang/Models/QuanLyBanHangContext.cs
@@ -25,10 +25,6 @@ public partial class QuanLyBanHangContext : DbContext
 
     public virtual DbSet<SanPham> SanPhams { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-VBSKBE8S;Database=QuanLyBanHang;Trusted_Connection=True;TrustServerCertificate=True;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<ChiTietDonHang>(entity =>
--- HoVanHoang_WebBanHang/Program.cs
+++ HoVanHoang_WebBanHang/Program.cs
@@ -1,11 +1,15 @@
+using HoVanHoang_WebBanHang.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<QuanLyBanHangContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");

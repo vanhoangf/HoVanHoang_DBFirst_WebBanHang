2c7593b062265a343d0af795806a83cf1f454627
Hồ Hoàng <hovanhoang959@gmail.com>
2026-04-07 22:12:38 +0700

    Project sau khi tao Controller va tao View

---
+++ HoVanHoang_WebBanHang/Controllers/SanPhamController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HoVanHoang_WebBanHang.Models;
+
+namespace HoVanHoang_WebBanHang.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        private readonly QuanLyBanHangContext _context;
+        public SanPhamController(QuanLyBanHangContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var danhSachSanPham = _context.SanPhams.Include(s => s.DanhMuc).ToList();
+            return View(danhSachSanPham);
+        }
+    }
+}
